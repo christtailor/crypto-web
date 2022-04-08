a4679f63348d2d330454606de6c1ae06ce8ebec5
Christ Tailor <ctailor0001@kctcs.edu>
2022-04-08 16:49:49 -0500

    commit on calc

--- src/components/testings/Testings.jsx
+++ src/components/testings/Testings.jsx
@@ -8,52 +8,35 @@ const Testings = () => {
   const [inputValue, getInputValue] = useState(0)
   const [cryptoValue, getCryptoValue] = useState('bitcoin')
   const [currencyValue, getCurrencyValue] = useState('usd')
-  
-  // console.log(inputValue,cryptoValue,currencyValue,calccoin)
-  
-  
-  
-  
+
         const inputvalueChanged = e => {
           getInputValue(e.target.value)
         }
         const CryptovalueChanged = e => {
           getCryptoValue(e.target.value)
-          const options = {
-            method: 'GET',
-            url: `https://api.coingecko.com/api/v3/coins/${cryptoValue}`,
-    
-          };
-          useEffect(() => {
+           
+        }
+        const currencyvalueChanged = e => {
+          getCurrencyValue(e.target.value)
+        }
+        const options = {
+          method: 'GET',
+          url: `https://api.coingecko.com/api/v3/coins/${cryptoValue}`,
+  
+        };
+        useEffect(() => {
+          async function Fetchdata() {
             axios.request(options)
             .then(res => {
                 getCryptodata(res.data)
               console.log(res.data)
             }).catch(error => console.log(error))
-          }, [])
-        }
-        const currencyvalueChanged = e => {
-          getCurrencyValue(e.target.value)
-        }
-      //   const currency_price = coin && coin.market_data && coin.market_data.current_price && coin.market_data.current_price.currencyValue.toLocaleString() 
-      //   console.log(currency_price)
+          }
+          Fetchdata()
+          }, [cryptoValue])
+
       let current_price = calccoin && calccoin.market_data && calccoin.market_data.current_price && calccoin.market_data.current_price[`${currencyValue}`] * inputValue
       console.log(current_price)
-      //   const Calculate = () =>{
-  
-      //   }
-      // const options = {
-      //   method: 'GET',
-      //   url: `https://api.coingecko.com/api/v3/coins/${cryptoValue}`,
-
-      // };
-
-      // axios.request(options).then(function (response) {
-      //   getCryptodata(response.data)
-      //   console.log(response.data)
-      // }).catch(function (error) {
-      //   console.error(error);
-      // });
 
     return (
       <>
@@ -61,7 +44,6 @@ const Testings = () => {
           <input 
                  type="text"
                  onChange={inputvalueChanged}
-              //    onChangeCapture={Calculate()}
             />
   
             <select name="crypto_currency" id="" onChange={CryptovalueChanged}>
